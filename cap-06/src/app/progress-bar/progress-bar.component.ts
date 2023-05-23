413f4ac73d4926d26d6050991d045b7f23a32509
dominedio <dominedib@outlook.it>
2023-05-23 15:48:44 +0200

    pa6

--- cap-06/src/app/progress-bar/progress-bar.component.ts
+++ cap-06/src/app/progress-bar/progress-bar.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from "@angular/core";
 @Component({
-  selector: 'app-progress-bar',
-  templateUrl: './progress-bar.component.html',
-  styleUrls: ['./progress-bar.component.css']
+  selector: "simple-progress-bar",
+  template: `
+    <div class="container">
+      <div class="progress" style="width: 20%">20%</div>
+    </div>
+  `,
+  styles: [
+    `
+      .container {
+        background-color: #ededed;
+        width: 400px;
+        border-radius: 24px;
+        position: relative;
+        margin: 20px auto;
+      }
+      .progress {
+        background-color: #10aded;
+        padding: 10px;
+        text-align: center;
+        font-weight: bold;
+        border-radius: inherit;
+        transition: width 0.5s ease-out;
+      }
+    `
+  ]
 })
-export class ProgressBarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class ProgressBar {}
